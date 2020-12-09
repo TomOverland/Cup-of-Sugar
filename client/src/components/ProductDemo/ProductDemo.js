d9c0e97855202a0ef866f362929557002fa6df8e
Tom Overland <tomoverland03@gmail.com>
2020-12-08 23:04:24 -0600

    added boilerplate to all undefined React components

--- client/src/components/ProductDemo/ProductDemo.js
+++ client/src/components/ProductDemo/ProductDemo.js
@@ -1,1 +1,8 @@
 // Landing Page Demo of Product
+import React from "react";
+
+const ProductDemo = () => {
+  return <div></div>;
+};
+
+export default ProductDemo;
--- client/src/components/SingleProductPage/SingleProductPage.js
+++ client/src/components/SingleProductPage/SingleProductPage.js
@@ -1,1 +1,8 @@
 // View of single product from results list
+import React from "react";
+
+const SingleProductPage = () => {
+  return <div></div>;
+};
+
+export default SingleProductPage;
