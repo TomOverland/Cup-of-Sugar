1956e234b1f70de1e9699aafaee850cd68e3413c
Nou Vang <nou.muas@gmail.com>
2020-12-09 18:25:44 -0600

    update banner and header js

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Banner from 'react-js-banner';
+
 
 const Banner = () => {
-  return <div></div>;
-};
+    return <div>
 
-export default Banner;
+<Banner 
+  title="Cup of Sugar Banner" 
+  image={"https://i.ibb.co/rcMChvK/Add-a-heading-1.png"} 
+  imageClass="App-logo"
+  css={this.state.banner2Css}
+/>
+
+    </div>;
+  };
+  
+  export default Banner;
+ 
