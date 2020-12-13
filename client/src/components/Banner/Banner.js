69e836c482db14342b54d8c5b7e7a4e4b237cd28
Tom Overland <tomoverland03@gmail.com>
2020-12-13 15:29:20 -0600

    added concurrent script to run client and app

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -1,18 +1,20 @@
 import React from "react";
 import SiteDescription from "../SiteDescription/SiteDescription";
 
-
-
 function Banner() {
-    return (
-        // eslint-disable-next-line
-        <div className="flex-row-reverse container mx-auto max-w-3xl object-none object-right-bottom md:object-top"><img className="w-full bg-right" src="https://media-public.canva.com/q-l3g/MADyQ2q-l3g/1/s3.jpg"></img>
-            <div className="absolute top-20 left-10">
-                <SiteDescription />
-            </div>
-        </div>
-    )
-};
-  
-  export default Banner;
- 
+  return (
+    // eslint-disable-next-line
+    <div className="flex-row-reverse container mx-auto max-w-3xl object-none object-right-bottom md:object-top">
+      <img
+        className="w-full bg-right"
+        src="https://media-public.canva.com/q-l3g/MADyQ2q-l3g/1/s3.jpg"
+        alt="Assorted items"
+      />
+      <div className="absolute top-20 left-10">
+        <SiteDescription />
+      </div>
+    </div>
+  );
+}
+
+export default Banner;
