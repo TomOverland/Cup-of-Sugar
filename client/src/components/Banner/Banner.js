b440910d24962ff1a04b77808b6704f210fc6068
Nou Vang <nou.muas@gmail.com>
2020-12-13 14:15:15 -0600

    update to jsx

--- client/src/components/Banner/Banner.js
+++ client/src/components/Banner/Banner.js
@@ -8,8 +8,8 @@ import SiteDescription from "../SiteDescription/SiteDescription";
 function Banner() {
     return (
         // eslint-disable-next-line
-        <div class="flex-row-reverse container mx-auto max-w-3xl object-none object-right-bottom md:object-top"><img className="w-full bg-right" src="https://media-public.canva.com/q-l3g/MADyQ2q-l3g/1/s3.jpg"></img>
-            <div class="absolute top-20 left-10">
+        <div className="flex-row-reverse container mx-auto max-w-3xl object-none object-right-bottom md:object-top"><img className="w-full bg-right" src="https://media-public.canva.com/q-l3g/MADyQ2q-l3g/1/s3.jpg"></img>
+            <div className="absolute top-20 left-10">
                 <SiteDescription />
 
             </div>
--- client/src/components/SiteDescription/SiteDescription.js
+++ client/src/components/SiteDescription/SiteDescription.js
@@ -4,18 +4,18 @@ import React from "react";
 const SiteDescription = () => {
   return (
   
-    <div class="flex items-center justify-end h-full max-w-sm bject-none object-left">
-      <div class="mt-5 box-border p-2 border-2 border-black">
-      <p class="font-black text-8xl">
+    <div className="flex items-center justify-end h-full max-w-sm bject-none object-left">
+      <div className="mt-5 box-border p-2 border-2 border-black">
+      <p className="font-black text-8xl">
         CUP OF SUGAR
       </p>
       <br />
-      <p class="font-black text-2xl">
+      <p className="font-black text-2xl">
         RENT. LEND. POST. REPEAT
       </p>
 
       <br />
-        <p class="font-serif">
+        <p className="font-serif">
         
           Welcome to Cup of Sugar! Cup of Sugar is a Twin Cities based peer-to-peer rental platform where users can both lend and borrow various items and
           products. While allowing product owners to earn a return on rentable items, renters can easily borrow products at
