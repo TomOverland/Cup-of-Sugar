2eeeab13badeaed591af920e8e2f367623ac78fd
Tom Overland <tomoverland03@gmail.com>
2020-12-15 22:31:57 -0600

    commented out auth for filter testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Login from './views/Login';
 // We have a VIEWS and a COMPONENT Login.  We should figure out which we're going to use.
 // Probably similar issue with below.
-import Home from './views/Home';
+import Home from "./views/Home";
 // import Product from './views/Product';
-import PostProduct from './components/PostProduct/PostProduct';
+import PostProduct from "./components/PostProduct/PostProduct";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer"
-import Login from './views/Login';
-import SingleProductPage from "./components/SingleProductPage/SingleProductPage"
-import Contact from "./components/Contact/Contact"
+import Footer from "./components/Footer/Footer";
+import Login from "./views/Login";
+import SingleProductPage from "./components/SingleProductPage/SingleProductPage";
+import Contact from "./components/Contact/Contact";
 
 function App() {
   return (
     <div>
       <Router>
-       <Header />
+        <Header />
         <Switch>
           <Route exact path="/">
             <Login />
-            <Home />
+            {/*<Home /> */}
           </Route>
           <Route path="/home">
             <Home />
-          </Route >
+          </Route>
           <Route path="/postproduct">
             <PostProduct />
           </Route>
